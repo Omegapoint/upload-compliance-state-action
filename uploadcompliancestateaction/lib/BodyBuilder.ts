62c24e2250c0a0f721609ad43d290ad7b0ee2bf9
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-09-12 17:49:51 +0200

    process.env

--- uploadcompliancestateaction/lib/BodyBuilder.ts
+++ uploadcompliancestateaction/lib/BodyBuilder.ts
@@ -9,49 +9,93 @@ let urls: string = '';
 
 export class BodyBuilder {
   createBody(teamName: string, repositoryId: string, codeRepositoryName: string, subscriptionId: string): ResponseBody {
-    let devopsOrgName: string = core.getInput('System.TeamFoundationCollectionUri');
-    const teamProjectName: string = core.getInput('System.TeamProject');
-    const allowedLocationPolicy: string = core.getInput('allowedLocationPolicy');
-    const secureScore: string = core.getInput('secureScore');
-    const numberOfDeployedVMs: string = core.getInput('numberOfDeployedVMs');
+    let devopsOrgName: string = process.env.System_TeamFoundationCollectionUri || '';
+const teamProjectName: string = process.env.System_TeamProject || '';
+const allowedLocationPolicy: string = process.env.allowedLocationPolicy || '';
+const secureScore: string = process.env.secureScore || '';
+const numberOfDeployedVMs: string = process.env.numberOfDeployedVMs || '';
 
-    const threatModelingDate: string = core.getInput('threatModelingDate');
-    const scaTool: string = core.getInput('scaTool');
-    const sastTool: string = core.getInput('sastTool');
-    const pentestDate: string = core.getInput('pentestDate');
-    const codeQualityTool: string = core.getInput('codeQualityTool');
-    const numberOfExposedSecrets: string = core.getInput('numberOfExposedSecrets');
-    const numberOfReviewers: string = core.getInput('numberOfReviewers');
-    const branchPolicyUpdateDate: string = core.getInput('branchPolicyUpdateDate');
-    const branchPolicyUpdateEmail: string = core.getInput('branchPolicyUpdateEmail');
+const threatModelingDate: string = process.env.threatModelingDate || '';
+const scaTool: string = process.env.scaTool || '';
+const sastTool: string = process.env.sastTool || '';
+const pentestDate: string = process.env.pentestDate || '';
+const codeQualityTool: string = process.env.codeQualityTool || '';
+const numberOfExposedSecrets: string = process.env.numberOfExposedSecrets || '';
+const numberOfReviewers: string = process.env.numberOfReviewers || '';
+const branchPolicyUpdateDate: string = process.env.branchPolicyUpdateDate || '';
+const branchPolicyUpdateEmail: string = process.env.branchPolicyUpdateEmail || '';
 
-    const compliantResources: string = core.getInput('compliantResources');
-    const nonCompliantResources: string = core.getInput('nonCompliantResources');
+const compliantResources: string = process.env.compliantResources || '';
+const nonCompliantResources: string = process.env.nonCompliantResources || '';
 
-    const scaNumberOfSeverity1: string = core.getInput('SCAnumberOfSeverity1');
-    const scaNumberOfSeverity2: string = core.getInput('SCAnumberOfSeverity2');
-    const scaNumberOfSeverity3: string = core.getInput('SCAnumberOfSeverity3');
-    const scaNumberOfSeverity4: string = core.getInput('SCAnumberOfSeverity4');
+const scaNumberOfSeverity1: string = process.env.SCAnumberOfSeverity1 || '';
+const scaNumberOfSeverity2: string = process.env.SCAnumberOfSeverity2 || '';
+const scaNumberOfSeverity3: string = process.env.SCAnumberOfSeverity3 || '';
+const scaNumberOfSeverity4: string = process.env.SCAnumberOfSeverity4 || '';
 
-    const cqNumberOfSeverity1: string = core.getInput('CQnumberOfSeverity1');
-    const cqNumberOfSeverity2: string = core.getInput('CQnumberOfSeverity2');
-    const cqNumberOfSeverity3: string = core.getInput('CQnumberOfSeverity3');
-    const cqNumberOfSeverity4: string = core.getInput('CQnumberOfSeverity4');
-    const cqNumberOfSeverity5: string = core.getInput('CQnumberOfSeverity5');
+const cqNumberOfSeverity1: string = process.env.CQnumberOfSeverity1 || '';
+const cqNumberOfSeverity2: string = process.env.CQnumberOfSeverity2 || '';
+const cqNumberOfSeverity3: string = process.env.CQnumberOfSeverity3 || '';
+const cqNumberOfSeverity4: string = process.env.CQnumberOfSeverity4 || '';
+const cqNumberOfSeverity5: string = process.env.CQnumberOfSeverity5 || '';
 
-    const sastNumberOfSeverity1: string = core.getInput('SASTnumberOfSeverity1');
-    const sastNumberOfSeverity2: string = core.getInput('SASTnumberOfSeverity2');
-    const sastNumberOfSeverity3: string = core.getInput('SASTnumberOfSeverity3');
+const sastNumberOfSeverity1: string = process.env.SASTnumberOfSeverity1 || '';
+const sastNumberOfSeverity2: string = process.env.SASTnumberOfSeverity2 || '';
+const sastNumberOfSeverity3: string = process.env.SASTnumberOfSeverity3 || '';
 
-    const tmNumberOfActiveTickets: string = core.getInput('tmNumberOfActiveTickets');
-    const tmNumberOfClosedTickets: string = core.getInput('tmNumberOfClosedTickets');
+const tmNumberOfActiveTickets: string = process.env.tmNumberOfActiveTickets || '';
+const tmNumberOfClosedTickets: string = process.env.tmNumberOfClosedTickets || '';
 
-    const ptNumberOfActiveTickets: string = core.getInput('ptNumberOfActiveTickets');
-    const ptNumberOfClosedTickets: string = core.getInput('ptNumberOfClosedTickets');
+const ptNumberOfActiveTickets: string = process.env.ptNumberOfActiveTickets || '';
+const ptNumberOfClosedTickets: string = process.env.ptNumberOfClosedTickets || '';
+
+const numUserInProdSeverity1: string = process.env.numUserInProdSeverity1 || '';
+const numUserInProdSeverity2: string = process.env.numUserInProdSeverity2 || '';
+const numUserInProdSeverity3: string = process.env.numUserInProdSeverity3 || '';
+
+    // let devopsOrgName: string = core.getInput('System.TeamFoundationCollectionUri');
+    // const teamProjectName: string = core.getInput('System.TeamProject');
+    // const allowedLocationPolicy: string = core.getInput('allowedLocationPolicy');
+    // const secureScore: string = core.getInput('secureScore');
+    // const numberOfDeployedVMs: string = core.getInput('numberOfDeployedVMs');
+
+    // const threatModelingDate: string = core.getInput('threatModelingDate');
+    // const scaTool: string = core.getInput('scaTool');
+    // const sastTool: string = core.getInput('sastTool');
+    // const pentestDate: string = core.getInput('pentestDate');
+    // const codeQualityTool: string = core.getInput('codeQualityTool');
+    // const numberOfExposedSecrets: string = core.getInput('numberOfExposedSecrets');
+    // const numberOfReviewers: string = core.getInput('numberOfReviewers');
+    // const branchPolicyUpdateDate: string = core.getInput('branchPolicyUpdateDate');
+    // const branchPolicyUpdateEmail: string = core.getInput('branchPolicyUpdateEmail');
+
+    // const compliantResources: string = core.getInput('compliantResources');
+    // const nonCompliantResources: string = core.getInput('nonCompliantResources');
+
+    // const scaNumberOfSeverity1: string = core.getInput('SCAnumberOfSeverity1');
+    // const scaNumberOfSeverity2: string = core.getInput('SCAnumberOfSeverity2');
+    // const scaNumberOfSeverity3: string = core.getInput('SCAnumberOfSeverity3');
+    // const scaNumberOfSeverity4: string = core.getInput('SCAnumberOfSeverity4');
+
+    // const cqNumberOfSeverity1: string = core.getInput('CQnumberOfSeverity1');
+    // const cqNumberOfSeverity2: string = core.getInput('CQnumberOfSeverity2');
+    // const cqNumberOfSeverity3: string = core.getInput('CQnumberOfSeverity3');
+    // const cqNumberOfSeverity4: string = core.getInput('CQnumberOfSeverity4');
+    // const cqNumberOfSeverity5: string = core.getInput('CQnumberOfSeverity5');
+
+    // const sastNumberOfSeverity1: string = core.getInput('SASTnumberOfSeverity1');
+    // const sastNumberOfSeverity2: string = core.getInput('SASTnumberOfSeverity2');
+    // const sastNumberOfSeverity3: string = core.getInput('SASTnumberOfSeverity3');
+
+    // const tmNumberOfActiveTickets: string = core.getInput('tmNumberOfActiveTickets');
+    // const tmNumberOfClosedTickets: string = core.getInput('tmNumberOfClosedTickets');
+
+    // const ptNumberOfActiveTickets: string = core.getInput('ptNumberOfActiveTickets');
+    // const ptNumberOfClosedTickets: string = core.getInput('ptNumberOfClosedTickets');
     
-    const numUserInProdSeverity1: string = core.getInput('numUserInProdSeverity1');
-    const numUserInProdSeverity2: string = core.getInput('numUserInProdSeverity2');
-    const numUserInProdSeverity3: string = core.getInput('numUserInProdSeverity3');
+    // const numUserInProdSeverity1: string = core.getInput('numUserInProdSeverity1');
+    // const numUserInProdSeverity2: string = core.getInput('numUserInProdSeverity2');
+    // const numUserInProdSeverity3: string = core.getInput('numUserInProdSeverity3');
     console.log('devopsOrgName:', devopsOrgName);
     console.log('teamProjectName:', process.env.teamProjectName);
     console.log('allowedLocationPolicy:', process.env.allowedLocationPolicy);
