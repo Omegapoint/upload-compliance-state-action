b290b60cd99463220843cc0f7747ec0bd989758e
Johanna Ahlskog <johanna.ahlskog@omegapoint.com>
2023-09-12 13:46:55 +0200

    changed back

--- uploadcompliancestateaction/js_out/lib/ResponseBody.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResponseBody = void 0;
-class ResponseBody {
-}
-exports.ResponseBody = ResponseBody;
--- uploadcompliancestateaction/js_out/lib/UrlBody.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UrlBody = void 0;
-class UrlBody {
-}
-exports.UrlBody = UrlBody;
