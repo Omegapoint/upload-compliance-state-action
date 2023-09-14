8a9ce52a05d5d504ec014e112a4f913df7f513ab
jonathanbokvad <runner@fv-az717-231.hmwa1bejwhke5akgd3yhbm2qnh.cx.internal.cloudapp.net>
2023-09-14 07:58:18 +0000

    Automated Build

---
+++ dist/lib/ResponseBody.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ResponseBody = void 0;
+class ResponseBody {
+    teamName;
+    teamProjectName;
+    codeRepositoryName;
+    subscriptionId;
+    threatModelingDate;
+    tmNumberOfActiveTickets;
+    tmNumberOfClosedTickets;
+    numberOfReviewers;
+    branchPolicyUpdateDate;
+    branchPolicyUpdateEmail;
+    scaTool;
+    scaNumberOfSeverity1;
+    scaNumberOfSeverity2;
+    scaNumberOfSeverity3;
+    scaNumberOfSeverity4;
+    sastTool;
+    sastNumberOfSeverity1;
+    sastNumberOfSeverity2;
+    sastNumberOfSeverity3;
+    secureScore;
+    allowedLocationPolicy;
+    compliantResources;
+    nonCompliantResources;
+    pentestDate;
+    ptNumberOfActiveTickets;
+    ptNumberOfClosedTickets;
+    numberOfDeployedVMs;
+    numberOfExposedSecrets;
+    codeQualityTool;
+    cqNumberOfSeverity1;
+    cqNumberOfSeverity2;
+    cqNumberOfSeverity3;
+    cqNumberOfSeverity4;
+    cqNumberOfSeverity5;
+    numUserInProdSeverity1;
+    numUserInProdSeverity2;
+    numUserInProdSeverity3;
+}
+exports.ResponseBody = ResponseBody;
+//# sourceMappingURL=ResponseBody.js.map
