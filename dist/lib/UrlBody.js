f122a7855b2a1d73e75709d07694f096b641b838
jonathanbokvad <runner@fv-az1114-377.v3gn4mkxnddubagvm3aoiftxed.cx.internal.cloudapp.net>
2023-09-14 14:35:17 +0000

    Automated Build

---
+++ dist/lib/UrlBody.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UrlBody = void 0;
+class UrlBody {
+    threatModelingDate;
+    numberOfReviewers;
+    scaTool;
+    sastTool;
+    secureScore;
+    allowedLocationPolicy;
+    pentestDate;
+    numberOfDeployedVMs;
+    numberOfExposedSecrets;
+    codeQualityTool;
+    usersInProduction;
+}
+exports.UrlBody = UrlBody;
+//# sourceMappingURL=UrlBody.js.map
