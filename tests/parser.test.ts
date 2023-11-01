de6047f74320a6febb7730db3ae085e49eaee4ff
Niklas Lennerdahl <niklas.lennerdahl@omegapoint.se>
2023-11-01 16:26:41 +0100

    add test for parser

---
+++ tests/parser.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import {parseToNumberOrUndefined} from '../src/lib/parser';
+
+describe('Parser Test', () => {
+    it('Shoould return number', () => {
+        const result: number | undefined = parseToNumberOrUndefined('1');
+        expect(result).to.be.equals(1)
+    })
+
+    const values: string[] = [' ', 'hej', null, undefined]
+    values.forEach((value: string) => {
+        it('Shoould return undefined', () => {
+            const result: number | undefined = parseToNumberOrUndefined(value);
+            expect(result).to.be.undefined;
+        })
+    })
+})
