6c566146029815372a973b2665dff7e874acf162
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2025-02-21 11:50:39 +0100

    changed format

--- src/lib/UrlBody.ts
+++ src/lib/UrlBody.ts
@@ -1,14 +1,14 @@
 export class UrlBody {
-  threatModelingDate: string;
-  numberOfReviewers: string;
-  scaTool: string;
-  sastTool: string;
-  secureScore: string;
-  allowedLocationPolicy: string;
-  pentestDate: string;
-  numberOfDeployedVMs: string;
-  secretScanningTool: string;
-  codeQualityTool: string;
-  usersInProduction: string;
-  userAccessToCode: string;
+    threatModelingDate: string;
+    numberOfReviewers: string;
+    scaTool: string;
+    sastTool: string;
+    secureScore: string;
+    allowedLocationPolicy: string;
+    pentestDate: string;
+    numberOfDeployedVMs: string;
+    secretScanningTool: string;
+    codeQualityTool: string;
+    usersInProduction: string;
+    userAccessToCode: string;
 }
